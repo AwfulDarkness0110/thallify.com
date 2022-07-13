a6f2fbe6473cc225c4db8d8129c5c1de38b41cf5
DarkMGarner <markgarner337@gmail.com>
2022-07-12 20:01:43 -0400

    nav

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import { useLocation } from "react-router-dom"
 import { saveAs } from 'file-saver'
 import html2canvas from "html2canvas"
@@ -11,6 +11,28 @@ import { analytics } from '../../firebase';
 const Nav = ({active, setTimeRange, setLayout, layout, setItemLimit, itemLimit, maxItemLimit}) => {
     const location = useLocation()
     const [isSaving, setIsSaving] = useState(false)
+    const activeRef = useRef(null);
+    const indicatorRef = useRef(null);
+    const [indicatorWidth, setIndicatorWidth] = useState(0);
+    const [indicatorLeft, setIndicatorLeft] = useState(0);
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowWidth(window.innerWidth)
+        })
+    }, [])
+
+    useEffect(() => {
+        const activeTab = activeRef.current;
+        const indicator = indicatorRef.current;
+        if (indicator && activeTab) {
+            const activeTabWidth = activeTab.offsetWidth;
+            const activeTabLeft = activeTab.offsetLeft;
+            setIndicatorWidth(activeTabWidth);
+            setIndicatorLeft(activeTabLeft);
+        }
+    }, [active, windowWidth])
 
 
     const downloadImage = () => {
@@ -58,22 +80,31 @@ const Nav = ({active, setTimeRange, setLayout, layout, setItemLimit, itemLimit, 
                 {!location.pathname.includes('/recently-played') && (
                 <>
                 <div 
+                    ref={active === 'short_term' ? activeRef : null}
                     onClick={() => setTimeRange('short_term')}
                     className={`nav-item${active === 'short_term' ? ' active' : '' }`}>
                     Last 7 Days
                 </div>
                 <div 
+                    ref={active === 'medium_term' ? activeRef : null}
                     onClick={() => setTimeRange('medium_term')}
                     className={`nav-item${active === 'medium_term' ? ' active' : '' }`}>
                     Last 6 months
                 </div>
                 <div 
+                    ref={active === 'long_term' ? activeRef : null}
                     onClick={() => setTimeRange('long_term')}
                     className={`nav-item${active === 'long_term' ? ' active' : '' }`}>
                     All time
                 </div>
                 </>
                 )}
+                <span className="tabs-indicator" ref={indicatorRef}
+                    style={{
+                        width: indicatorWidth,
+                        left: indicatorLeft,
+                    }}
+                />
             </div>
             <div className="nav-right">
                 <div className={`nav-item`}>
